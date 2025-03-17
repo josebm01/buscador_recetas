98cedbc312b225debe8bc8279c38173e03e1a81f
Jose BM <jbaxin@manuelsolis.com>
2025-03-16 19:07:05 -0600

    Information is added to the modal, including the recipe details and ingredients

--- js/app.js
+++ js/app.js
@@ -66,6 +66,10 @@ initApp = () => {
             const recipeButton = document.createElement('BUTTON')
             recipeButton.classList.add('btn', 'btn-danger', 'w-100')
             recipeButton.textContent = 'Ver receta'
+            
+            recipeButton.onclick = function() {
+                selectRecipe(idMeal)
+            }
 
 
             // Inyectamos en el código HTML
@@ -80,6 +84,75 @@ initApp = () => {
         })
     } 
 
+    const selectRecipe = ( id ) => {
+        const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+        
+        fetch( url )
+            .then( response => response.json())
+            .then( result => showRecipeModal(result.meals[0]) )
+    }
+
+    const showRecipeModal = ( recipe ) => {
+        
+        const { idMeal, strInstructions, strMeal, strMealThumb } = recipe 
+
+        // Añadir contenido al modal 
+        const modalTitle = document.querySelector('.modal .modal-title')
+        const modalBody = document.querySelector('.modal .modal-body')
+
+        modalTitle.textContent = strMeal
+
+        modalBody.innerHTML = `
+            <img class="img-fluid" src="${strMealThumb}" alt="receta ${strMeal}" />
+            <h3 class"my-3 mt-5">Instrucciones</h3>
+            <p>${strInstructions}</p>
+            <h3 class="mt-3">Ingredientes y cantidades</h3>
+        `
+
+        const listGroup = document.createElement('UL')
+        listGroup.classList.add('list-group')
+
+        // Mostrar cantidades e ingredientes
+        for (let i = 1; i < 20; i++) {
+            if ( recipe[`strIngredient${i}`] ){
+                const ingredient = recipe[`strIngredient${i}`]
+                const measure = recipe[`strMeasure${i}`]
+
+                // Lista de ingredientes
+                const ingredientList = document.createElement('LI')
+                ingredientList.classList.add('list-group-item')
+                ingredientList.textContent = `#${i}: ${ingredient} - ${measure}`
+                
+                // Agreamos a la lista
+                listGroup.appendChild(ingredientList)
+            }            
+        }
+
+        modalBody.appendChild(listGroup)
+
+        const modalFooter = document.querySelector('.modal-footer')
+        cleanHTML(modalFooter)
+
+        // Botones de cerrar y favorito
+        const btnFavorite = document.createElement('BUTTON')
+        btnFavorite.classList.add('btn', 'btn-danger', 'col')
+        btnFavorite.textContent = 'Guardar favorito'
+        
+        const btnCloseModal = document.createElement('BUTTON')
+        btnCloseModal.classList.add('btn', 'btn-secondary', 'col')
+        btnCloseModal.textContent = 'Cerrar'
+        // Cerrar modal con la instancia
+        btnCloseModal.onclick = function() {
+            modal.hide()
+        }
+
+        modalFooter.appendChild(btnFavorite)
+        modalFooter.appendChild(btnCloseModal)
+
+        // muestra el modal 
+        modal.show()
+    }
+
     const cleanHTML = ( select ) => {
         while( select.firstChild ) {
             select.removeChild(select.firstChild)
@@ -90,6 +163,8 @@ initApp = () => {
     selectCategories.addEventListener('change', selectCategory)
 
     const result = document.querySelector('#resultado')
+    // Creación del modal
+    const modal = new bootstrap.Modal('#modal', {})
 
 
 
